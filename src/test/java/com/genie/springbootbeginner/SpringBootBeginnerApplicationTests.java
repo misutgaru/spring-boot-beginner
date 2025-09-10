e25c5196e84f73060e22387759ac9acc7bd57fc7
misutgaru <w-w@kakao.com>
2025-09-10 09:56:39 +0900

    REST API GET TEST

---
+++ src/test/java/com/genie/springbootbeginner/SpringBootBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genie.springbootbeginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBeginnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
