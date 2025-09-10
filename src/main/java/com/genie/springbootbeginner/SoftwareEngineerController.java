38485fd2279c8bbc4affb420c847468b00a43749
misutgaru <w-w@kakao.com>
2025-09-10 16:41:42 +0900

    Postgres Install

---
+++ src/main/java/com/genie/springbootbeginner/SoftwareEngineerController.java
@@ -0,0 +1,29 @@
+package com.genie.springbootbeginner;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getSoftwareEngineers() {
+        return List.of(
+                new SoftwareEngineer(
+                        1,
+                        "James",
+                        "js, node, react, tailwindcss"
+                ),
+                new SoftwareEngineer(
+                        2,
+                        "Jamila",
+                        "java, spring, spring boot"
+                )
+        );
+    }
+
+}
